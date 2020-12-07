fa4a056b7188e476d1abb5484bfaffd9516aa17c
MDCYT <67084442+MDCYT@users.noreply.github.com>
2020-12-06 21:15:47 -0500

    Bear, fox, and pandas

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,7 +15,6 @@ router.get('/contact', (req, res) => {
 
 router.get('/cat', async (req, res) => {
 
-
     let cat = await utils.cat()
  
     res.render('cat.html', { image: cat })
@@ -23,10 +22,30 @@ router.get('/cat', async (req, res) => {
 
 router.get('/dog', async (req, res) => {
 
-
     let dog = await utils.dog()
  
     res.render('dog.html', { image: dog })
+});
+
+router.get('/bear', async (req, res) => {
+
+    let bear = await utils.bear()
+ 
+    res.render('bear.html', { image: bear })
+});
+
+router.get('/panda', async (req, res) => {
+
+    let panda = await utils.panda()
+ 
+    res.render('panda.html', { image: panda })
+});
+
+router.get('/fox', async (req, res) => {
+
+    let fox = await utils.fox()
+ 
+    res.render('fox.html', { image: fox })
 });
 
 module.exports= router
