458abdd9792e514ca216b66ee67f1f3751089fa1
MDCYT <67084442+MDCYT@users.noreply.github.com>
2020-12-06 20:47:06 -0500

    Cat and Dog

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express')
 const router = express.Router();
+const utils = require('discord-utilities-js')
+
 
 router.get('/', (req, res) => {
     res.render('index.html', { title: "Mi Primera Pagina" })
@@ -9,6 +11,22 @@ router.get('/', (req, res) => {
 
 router.get('/contact', (req, res) => {
     res.render('contact.html', { title: "Pagina de Contacto" })
+});
+
+router.get('/cat', async (req, res) => {
+
+
+    let cat = await utils.cat()
+ 
+    res.render('cat.html', { image: cat })
+});
+
+router.get('/dog', async (req, res) => {
+
+
+    let dog = await utils.dog()
+ 
+    res.render('dog.html', { image: dog })
 });
 
 module.exports= router
