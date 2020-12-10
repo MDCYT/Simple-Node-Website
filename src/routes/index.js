68b586ce42e963dc32306d6e665aead76ba439d6
MDCYT <67084442+MDCYT@users.noreply.github.com>
2020-12-09 19:29:08 -0500

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,63 +16,63 @@ router.get('/contact', (req, res) => {
 router.get('/cat', async (req, res) => {
 
     let cat = await utils.cat()
- 
+
     res.render('cat.html', { image: cat })
 });
 
 router.get('/dog', async (req, res) => {
 
     let dog = await utils.dog()
- 
+
     res.render('dog.html', { image: dog })
 });
 
 router.get('/bear', async (req, res) => {
 
     let bear = await utils.bear()
- 
+
     res.render('bear.html', { image: bear })
 });
 
 router.get('/panda', async (req, res) => {
 
     let panda = await utils.panda()
- 
+
     res.render('panda.html', { image: panda })
 });
 
 router.get('/fox', async (req, res) => {
 
     let fox = await utils.fox()
- 
+
     res.render('fox.html', { image: fox })
 });
 
 router.get('/bird', async (req, res) => {
 
     let bird = await utils.bird()
- 
+
     res.render('bird.html', { image: bird })
 });
 
 router.get('/koala', async (req, res) => {
 
     let koala = await utils.koala()
- 
+
     res.render('koala.html', { image: koala })
 });
 
 router.get('/redpanda', async (req, res) => {
 
     let redpanda = await utils.redpanda()
- 
+
     res.render('redpanda.html', { image: redpanda })
 });
 
 router.get('/binary', async (req, res) => {
 
     let redpanda = await utils.redpanda()
- 
+
     res.render('binary.html', { image: redpanda })
 });
 
@@ -83,7 +83,7 @@ router.get('/binary/convert', async (req, res) => {
     const tipo = req.query.type
 
     if (tipo === "Texto a Binario") {
-  
+
         function convertir(text) {
             var length = text.length,
                 output = [];
@@ -92,7 +92,7 @@ router.get('/binary/convert', async (req, res) => {
                 output.push(Array(8 - bin.length + 1).join("0") + bin);
             }
             return output.join(" ");
-        
+
         }
 
         var resultado = convertir(texto)
@@ -123,57 +123,74 @@ router.get('/binary/convert', async (req, res) => {
 
 router.get('/translate', async (req, res) => {
 
-    const translate = require('node-google-translate-skidz');
+    let translate = require('node-google-translate-skidz');
 
-    const texto = req.query.text
+    let texto = req.query.text
 
-    const tipo = req.query.entrada
+    let tipo = req.query.entrada
 
-    const salida = req.query.salida
-
-
-        translate({
-            text: texto || "No a ingresado Texto",
-            source: tipo || "es", 
-            target: salida || "es"
-        }, function (result) {
-                
-            const resultado = result.translation
-             
-        var textofinal = texto || "No a ingresado texto"
-
-        var input = "Texto Original"
-
-        var output = "Texto Traducido"
+    let salida = req.query.salida
 
 
-                res.render('translate.html', { respuesta: resultado, texto: textofinal, entrada: input, salida: output })
-            });
+    translate({
+        text: texto || "No a ingresado Texto",
+        source: tipo || "es",
+        target: salida || "es"
+    }, function (result) {
+
+        let resultado = result.translation
+
+        let textofinal = texto || "No a ingresado texto"
+
+        let input = "Texto Original"
+
+        let output = "Texto Traducido"
+
+
+        res.render('translate.html', { respuesta: resultado, texto: textofinal, entrada: input, salida: output })
+    });
 });
 
 router.get('/translate.json', async (req, res) => {
 
-    const translate = require('node-google-translate-skidz');
+    let translate = require('node-google-translate-skidz');
 
-    const texto = req.query.text
+    let idiomas = require('../public/json/idiomas.json')
 
-    if(!texto) return res.json({"error": "No a puesto ningun texto, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en"})
+    let texto = req.query.text
 
-    const tipo = req.query.entrada
+    if (!texto) return res.json({ "error": "No a puesto ningun texto, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
 
-    const salida = req.query.salida
+    let tipo = req.query.entrada
 
+    if (!tipo) return res.json({ "error": "No a puesto ningun idioma de entrada, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
 
-        translate({
-            text: texto || "No a ingresado Texto",
-            source: tipo || "es", 
-            target: salida || "es"
-        }, function (result) {
-                
-            const resultado = result.translation
+    if (!tipo.includes(idiomas)) return res.json({ "error": "El idioma de entrada que ha puesto no esta en nuestra lista de idiomas validos, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
 
-                res.json({"Respuesta": resultado})
-            });
+    if (tipo.length != 2) return res.json({ "error": "El idioma de entrada no es valido, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
+
+    let salida = req.query.salida
+
+    if (!salida) return res.json({ "error": "No a puesto ningun idioma de salida, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
+
+    if (!salida.includes(idiomas)) return res.json({ "error": "El idioma de salida que ha puesto no esta en nuestra lista de idiomas validos, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
+
+    if (salida.length != 2) return res.json({ "error": "El idioma de salida no es valido, ejemplo de uso correcto ?text=Hola&entrada=es&salida=en" })
+
+    translate({
+        text: texto || "No a ingresado Texto",
+        source: tipo || "es",
+        target: salida || "es"
+    }, function (result) {
+
+        let resultado = result.translation
+
+        res.json({ "Respuesta": resultado })
+    }).catch((error) => {
+
+        res.json({ "error": error })
+
+    })
 });
 
-module.exports= router
+module.exports = router
